33bc51a35a7c9a08a898f41fc32d27d9d4dd4e29
modabbir <modabbir276@gmail.com>
2024-09-02 01:00:39 +0600

    array first work done

---
+++ jsArray.js
@@ -0,0 +1,8 @@
+const numbers =[1 , 5, 66, 11, 45, 65];
+console.log(numbers.length);
+// get element value by index
+const fourth = numbers[3];
+console.log(fourth);
+// set or update elements value by index
+numbers[1] = 456;
+console.log(numbers);
